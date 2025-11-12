20eb5bda71592f18eb06b9d5faf04fb1ec9d55f0
Komsan Kanjanasit <94804188+komsan-k@users.noreply.github.com>
2025-11-12 17:01:10 +0700

    Create tb-nxor-ann.v

---
+++ verilog/xor-nn/tb-nxor-ann.v
@@ -0,0 +1,16 @@
+// tb_nxor_ann.v
+module tb_nxor_ann;
+    reg A, B;
+    wire Y;
+
+    nxor_ann uut (.A(A), .B(B), .Y(Y));
+
+    initial begin
+        $display("A B | Y");
+        A=0; B=0; #1; $display("%b %b | %b", A,B,Y);
+        A=0; B=1; #1; $display("%b %b | %b", A,B,Y);
+        A=1; B=0; #1; $display("%b %b | %b", A,B,Y);
+        A=1; B=1; #1; $display("%b %b | %b", A,B,Y);
+        $finish;
+    end
+endmodule
